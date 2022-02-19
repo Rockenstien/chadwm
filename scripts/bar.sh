d3c71bcab9bfebf55c27d80e1243308ea6520d67
Rockenstien <paras.klangyan@gmail.com>
2022-02-19 18:18:35 +0530

    Added few more patches

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/bash
 
 # ^c$var^ = fg color
 # ^b$var^ = bg color
@@ -8,23 +8,25 @@ interval=0
 # load colors
 . ~/.config/chadwm/scripts/bar_themes/onedark
 
-cpu() {
-	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+# cpu() {
+# 	cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
-	printf "^c$black^ ^b$green^ CPU"
-	printf "^c$white^ ^b$grey^ $cpu_val"
-}
+# 	printf "^c$black^ ^b$green^ CPU"
+# 	printf "^c$white^ ^b$grey^ $cpu_val"
+# }
 
-pkg_updates() {
-	updates=$(doas xbps-install -un | wc -l) # void
-	# updates=$(checkupdates | wc -l)   # arch , needs pacman contrib
-	# updates=$(aptitude search '~U' | wc -l)  # apt (ubuntu,debian etc)
+# pkg_updates() {
+# 	updates=$(checkupdates | wc -l)   # arch , needs pacman contrib
 
-	if [ -z "$updates" ]; then
-		printf "^c$green^  Fully Updated"
-	else
-		printf "^c$green^  $updates"" updates"
-	fi
+# 	if [ -z "$updates" ]; then
+# 		printf "^c$green^  Fully Updated"
+# 	else
+# 		printf "^c$green^  $updates"" updates"
+# 	fi
+# }
+
+spotify() {
+	printf "$(node /home/paras/.config/spotifyd/which_song.js)"
 }
 
 battery() {
@@ -44,8 +46,8 @@ mem() {
 
 wlan() {
 	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
-	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
+	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^" ;;
 	esac
 }
 
@@ -56,8 +58,8 @@ clock() {
 
 while true; do
 
-	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
-	interval=$((interval + 1))
+	# [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+	# interval=$((interval + 1))
 
-	sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
+	sleep 1 && xsetroot -name "$(spotify) $(mem) $(wlan) $(clock)"
 done
